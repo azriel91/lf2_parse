336f57feea546e25546bc9df09534a51faa8ce73
Azriel Hoh <azriel91@gmail.com>
2020-08-31 08:02:27 +1200

    Add `BPoint` type.

---
+++ src/element/b_point.rs
@@ -0,0 +1,8 @@
+/// Bleeding coordinates when the character has low HP.
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct BPoint {
+    /// X coordinate.
+    pub x: i32,
+    /// Y coordinate.
+    pub y: i32,
+}
