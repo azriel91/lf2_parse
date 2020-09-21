bc2655a25bcf8d51c15e660567aea175117999f7
Azriel Hoh <azriel91@gmail.com>
2020-09-21 14:17:04 +1200

    Add better error message for `EffectParseError`.

--- src/element/itr/effect/effect_parse_error.rs
+++ src/element/itr/effect/effect_parse_error.rs
@@ -13,9 +13,27 @@ impl Display for EffectParseError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             Self::ParseIntError(parse_int_error) => write!(f, "{}", parse_int_error),
-            Self::InvalidValue(value) => {
-                write!(f, "`{}` is not recognized as a valid `Effect` value.", value)
-            }
+            Self::InvalidValue(value) => write!(
+                f,
+                "`{}` is not recognized as a valid `Effect` value.\n\
+                \n\
+                Valid values are:\n\
+                \n\
+                - 0 (Normal)\n\
+                - 1 (Blood)\n\
+                - 2 (Fire)\n\
+                - 3 (Ice)\n\
+                - 4 (Reflect)\n\
+                - 5 (Reflects)\n\
+                - 20 (FireNoReburn)\n\
+                - 21 (FireBreath)\n\
+                - 22 (FireExplode)\n\
+                - 23 (PowerExplode)\n\
+                - 30 (Icicle)\n\
+                \n\
+                ",
+                value
+            ),
         }
     }
 }
